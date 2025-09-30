1d5f7ebfe881f7e2a81384f140eb7154c2e848f1
Anjanikumar <avadhanamanjan@gmail.com>
2025-09-29 21:29:11 -0500

    Added initial SQL deployment files

--- sql/new/query_tab.sql
+++ sql/new/query_tab.sql
@@ -1,6 +1,6 @@
 use mydb
 
-create table productbatch(
+create table productbatch1(
 id int primary key,
 name varchar(30),
 description varchar(100),
@@ -8,9 +8,9 @@ price double);
 
 select*from productbatch
 -- Insert first product
-INSERT INTO productbatch (id, name, description, price)
+INSERT INTO productbatch1 (id, name, description, price)
 VALUES (1, 'Laptop', '15-inch display with 8GB RAM', 55000.00);
 
 -- Insert second product
-INSERT INTO productbatch (id, name, description, price)
+INSERT INTO productbatch1 (id, name, description, price)
 VALUES (2, 'Smartphone', '6.5-inch screen with 128GB storage', 25000.00);
