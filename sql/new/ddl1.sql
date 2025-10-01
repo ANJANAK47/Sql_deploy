1d26ffbaed31c1fafc594cc6c97982f1742b76dc
A.ANJANI KUMAR <91489712+ANJANAK47@users.noreply.github.com>
2025-09-30 23:53:06 -0500

    now test2

--- sql/new/ddl1.sql
+++ sql/new/ddl1.sql
@@ -2,7 +2,7 @@ create table users(
 -- make a new table
 CREATE TABLE users (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(101) not null,
+  name VARCHAR(111) not null,
   email VARCHAR(170) UNIQUE
 );
 
