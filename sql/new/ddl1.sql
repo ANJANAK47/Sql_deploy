fcb14ad64203c752f78de8948a2b1790d6d843fb
A.ANJANI KUMAR <91489712+ANJANAK47@users.noreply.github.com>
2025-09-30 21:09:50 -0500

    testagain

--- sql/new/ddl1.sql
+++ sql/new/ddl1.sql
@@ -2,7 +2,7 @@ create table users(
 -- make a new table
 CREATE TABLE users (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100) not null,
+  name VARCHAR(101) not null,
   email VARCHAR(191) UNIQUE
 );
 
