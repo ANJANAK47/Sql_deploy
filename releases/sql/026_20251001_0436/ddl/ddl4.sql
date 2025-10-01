92c34423c505c66c8860e7f4f01c24908994c4fb
ANJANAK47 <91489712+ANJANAK47@users.noreply.github.com>
2025-10-01 04:36:45 +0000

    [skip ci] feat(sql): batch 026_20251001_0436 (concat changed ddl*/dml*)

---
+++ releases/sql/026_20251001_0436/ddl/ddl4.sql
@@ -0,0 +1,12 @@
+-- ===== BEGIN ddl1.sql =====
+create table users(
+-- make a new table
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(101) not null,
+  email VARCHAR(190) UNIQUE
+);
+
+
+-- ===== END ddl1.sql =====
+
